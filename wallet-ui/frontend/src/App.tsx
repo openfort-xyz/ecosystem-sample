60b0426792c7b061cfd87c265980e717a069f37a
Jaume Alavedra <jamalavedra@gmail.com>
2025-03-21 09:02:58 +0100

    chore: rm unnecessary auth

--- wallet-ui/frontend/src/App.tsx
+++ wallet-ui/frontend/src/App.tsx
@@ -6,7 +6,6 @@ import {
   EthSignTypedDataV4, 
   PersonalSign, 
   WalletShowCalls,
-  withAuthenticationRequired,
   Settings,
   UnsupportedMethod,
 } from '@openfort/ecosystem-js/react';
@@ -14,25 +13,19 @@ import { Route, Routes } from 'react-router-dom';
 import Loading from './Loading';
 
 
-const ProtectedRoute = ({ component, ...args }: any) => {
-  const Component = withAuthenticationRequired(component, {
-    onRedirecting: () => <Loading />,
-  });
-  return <Component {...args} />;
-};
 
 function App() {
   return (
     <Routes>
-      <Route path='/sign/personal-sign' element={<ProtectedRoute component={PersonalSign} />} />
-      <Route path='/sign/eth-sign-typed-data-v-4' element={<ProtectedRoute component={EthSignTypedDataV4} />} />
-      <Route path='/sign/eth-send-transaction' element={<ProtectedRoute component={EthSendTransaction} />} />
-      <Route path='/sign/wallet-grant-permissions' element={<ProtectedRoute component={WalletGrantPermissions} />} />
-      <Route path='/sign/wallet-show-calls' element={<ProtectedRoute component={WalletShowCalls} />} />
-      <Route path='/sign/wallet-send-calls' element={<ProtectedRoute component={WalletSendCalls} />} />
-      <Route path='/sign/eth-request-accounts' element={<ProtectedRoute component={EthRequestAccounts} />} />
-      <Route path='/settings' element={<ProtectedRoute component={Settings} />} />
-      <Route path='/' element={<ProtectedRoute component={Loading} />} />
+      <Route path='/sign/personal-sign' element={<PersonalSign />} />
+      <Route path='/sign/eth-sign-typed-data-v-4' element={<EthSignTypedDataV4 />} />
+      <Route path='/sign/eth-send-transaction' element={<EthSendTransaction />} />
+      <Route path='/sign/wallet-grant-permissions' element={<WalletGrantPermissions />} />
+      <Route path='/sign/wallet-show-calls' element={<WalletShowCalls />} />
+      <Route path='/sign/wallet-send-calls' element={<WalletSendCalls />} />
+      <Route path='/sign/eth-request-accounts' element={<EthRequestAccounts />} />
+      <Route path='/settings' element={<Settings />} />
+      <Route path='/' element={<Loading />} />
       
       <Route path='*' element={<UnsupportedMethod />} />
     </Routes>
