e0a1a7df74a571486a2cf00c5c0f4bd817c89b4d
Huguet57 <andreuhuguet@gmail.com>
2025-11-07 17:06:23 +0100

    feat: remove auto-connect

--- wallet-ui/portfolio/src/Home.tsx
+++ wallet-ui/portfolio/src/Home.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react'
-import { useAccount, useConnect } from 'wagmi'
+import { useAccount } from 'wagmi'
 import { ecosystemWalletInstance } from './lib/ecosystemWallet'
 
 import { Landing } from './components/Landing'
 import { Dashboard } from './components/Dashboard'
-import Loading from './components/Loading'
 
 export function Home() {
   const account = useAccount()
-  const { connectors, connect } = useConnect()
-  const [isAutoConnecting, setIsAutoConnecting] = React.useState(true)
-  const hasAttemptedConnect = React.useRef(false)
 
   // Initialize ecosystem wallet iframe
   React.useEffect(() => {
@@ -40,30 +36,6 @@ export function Home() {
     setTimeout(ensureCorrectChain, 500);
   }, []);
 
-  // Auto-connect if there's an existing session
-  React.useEffect(() => {
-    if (!hasAttemptedConnect.current && !account.isConnected && connectors.length > 0) {
-      const injectedConnector = connectors.find(
-        (connector) => connector.id === 'com.rapidfire.id'
-      )
-      if (injectedConnector) {
-        hasAttemptedConnect.current = true
-        connect({ connector: injectedConnector })
-        // Give it a moment to connect, then stop loading
-        setTimeout(() => {
-          setIsAutoConnecting(false)
-        }, 500)
-      } else {
-        setIsAutoConnecting(false)
-      }
-    } else if (account.isConnected) {
-      setIsAutoConnecting(false)
-    } else if (connectors.length > 0 && hasAttemptedConnect.current) {
-      setIsAutoConnecting(false)
-    }
-  }, [account.isConnected, connectors, connect])
-
-  if (isAutoConnecting) return <Loading />
   if (!account.isConnected) return <Landing />
   return <Dashboard />
 }
